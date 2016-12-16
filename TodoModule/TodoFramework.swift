15eabed4e9669feadfbe1e4434b2a4d007a1269e
afsara <afsarunnisa@nbostech.com>
2016-12-16 17:55:19 +0530

    minor changes

--- TodoModule/TodoFramework.swift
+++ TodoModule/TodoFramework.swift
@@ -14,9 +14,9 @@ public class TodoFramework {
     public static func performSegueTodoModule(caller: UIViewController) {
         let podBundle = Bundle(for: TodoFramework.self)
         
-        let bundleURL = podBundle.url(forResource: "TodoModule", withExtension: "bundle")
-        let bundle = Bundle(url: bundleURL!)!
-        let storyboard = UIStoryboard(name: "TodoModule", bundle: bundle)
+//        let bundleURL = podBundle.url(forResource: "TodoModule", withExtension: "bundle")
+//        let bundle = Bundle(url: bundleURL!)!
+        let storyboard = UIStoryboard(name: "TodoModule", bundle: nil)
         let vc = storyboard.instantiateInitialViewController()!
         caller.present(vc, animated: true, completion: nil)
     }
